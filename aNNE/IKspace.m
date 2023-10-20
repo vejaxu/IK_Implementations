80bf01a6ad0f9ca5538031556eed34bcbf9aaf6f
forleast330 <49986719+forleast330@users.noreply.github.com>
2023-10-20 15:50:52 +0800

    Add files via upload

---
+++ aNNE/IKspace.m
@@ -0,0 +1,18 @@
+function [ndata] = IKspace (Sdata,data, psi, t)
+% mapping data to Isolation Kernel space 
+% Sdata is used for partition
+
+[sn,~]=size(Sdata);  
+[n,d]=size(data);  
+ndata=[];
+c=0:psi:(n-1)*psi;
+for i = 1:t 
+    subIndex = datasample(1:sn, psi, 'Replace', false);
+    tdata=Sdata(subIndex,:);      
+    dis=pdist2(tdata,data);    
+    [~, centerIdx] = min(dis);
+    z=zeros(psi,n);
+    z(centerIdx+c)=1;
+    ndata=[ndata z']; 
+end 
+end
