181fa8834c63852df4fc7a7efd1ec2d302f44764
weijie xu <wjxu29@gmail.com>
2024-10-25 14:43:51 +0800

    Update iNNE_IK.py

--- iNNE/iNNE_IK.py
+++ iNNE/iNNE_IK.py
@@ -6,35 +6,47 @@ class iNN_IK:
     data = None
     centroid = []
     def __init__(self, psi, t):
-        self.psi = psi
-        self.t = t
+        self.psi = psi # size of subsampling
+        self.t = t # iterations
 
     def fit_transform(self, data):
         self.data = data
-        self.centroid = []
-        self.centroids_radius = []
+        self.centroid = [] # centroid should have t elements and each element has psi elements
+        self.centroids_radius = [] # centroids_radius's shape is equal to centroid's shape
         sn = self.data.shape[0]
         n, d = self.data.shape
-        IDX = np.array([])  #column index
+        IDX = np.array([])  # column index
         V = []
         for i in range(self.t):
-            subIndex = sample(range(sn), self.psi)
-            self.centroid.append(subIndex)
+            subIndex = sample(range(sn), self.psi) # return a list of unique elements randomly chosen from the given range
+            
+            self.centroid.append(subIndex) # its shape is t * psi
             tdata = self.data[subIndex, :]
             tt_dis = cdist(tdata, tdata)
-            radius = [] #restore centroids' radius
+            
+            # the iteration is to calculate each point's radius
+            radius = []
             for r_idx in range(self.psi):
                 r = tt_dis[r_idx]
                 r[r<0] = 0
                 r = np.delete(r,r_idx)
                 radius.append(np.min(r))
-            self.centroids_radius.append(radius)
+
+            self.centroids_radius.append(radius) # its shape is t * psi
+
+            # for each point in data, find its nearest neighobur in tdata
             nt_dis = cdist(tdata, self.data)
             centerIdx = np.argmin(nt_dis, axis=0)
+
+            # build a list V where each element is 1 or 0 to represent whether each point in data is within the radius of its nearest neighbor in tdata
             for j in range(n):
-                V.append(int(nt_dis[centerIdx[j],j] <= radius[centerIdx[j]]))
-            IDX = np.concatenate((IDX, centerIdx + i * self.psi), axis=0)
-        IDR = np.tile(range(n), self.t) #row index
+                V.append(int(nt_dis[centerIdx[j],j] <= radius[centerIdx[j]])) 
+    
+            IDX = np.concatenate((IDX, centerIdx + i * self.psi), axis=0) # unique
+
+        IDR = np.tile(range(n), self.t) # shape = n * t
+        # when n = 5, t = 3, IDR = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
+
         #V = np.ones(self.t * n) #value
         ndata = csr_matrix((V, (IDR, IDX)), shape=(n, self.t * self.psi))
         return ndata
